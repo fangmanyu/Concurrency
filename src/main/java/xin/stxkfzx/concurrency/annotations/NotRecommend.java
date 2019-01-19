4ec036c2bc4740ad2f4c37bca1f014ff028c1711
fmy <592618473@qq.com>
2019-01-19 10:42:06 +0800

    add annotations

---
+++ src/main/java/xin/stxkfzx/concurrency/annotations/NotRecommend.java
@@ -0,0 +1,18 @@
+package xin.stxkfzx.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记【不推荐】的类或写法
+ *
+ * @author fmy
+ * @date 2019-01-19 10:34
+ */
+@Target(ElementType.TYPE) // 标记到类中
+@Retention(RetentionPolicy.RUNTIME) //编译时忽略
+public @interface NotRecommend {
+    String value() default "";
+}
